a91c20f723a606f96e71ed385ab5b2b8e8bdefd1
ritikrajdev <ritikrajdev761@gmail.com>
2021-03-14 03:38:51 +0530

    Ranger show images using kitty protocol only

--- .shell/POSIX/general_alias.sh
+++ .shell/POSIX/general_alias.sh
@@ -1,8 +1,6 @@
 alias ls='ls --color=auto'
 alias vim=nvim
 alias grep='grep --colour=auto'
-alias icat="kitty +kitten icat"
-alias kdiff="kitty +kitten diff"
 alias diff="diff --color"
 alias ll="exa -lg"
 alias tree="exa --tree"
@@ -11,3 +9,6 @@ alias valgrindl='valgrind --track-origins=yes --leak-check=full --show-leak-kind
 alias g++="g++ -std=c++14 -Wall"
 alias pyma="python manage.py"
 
+alias ssh="kitty +kitten ssh"
+alias kdiff="kitty +kitten diff"
+alias icat="kitty +kitten icat"
