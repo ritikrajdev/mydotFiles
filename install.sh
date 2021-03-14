1b94343ce79377778228bbeba7d5bca23e1c2336
ritikrajdev <ritikrajdev761@gmail.com>
2021-03-14 07:47:44 +0530

    install script update with accept all parameter -y

--- install.sh
+++ install.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 
 main() {
+	local ask_permission="i"
+
+	if [ "$1" = "-y" ]
+	then
+		ask_permission="f"
+	fi
 
 	local current_directory=$(dirname $(realpath $0))
 
-	echo $current_directory
+	ln -s${ask_permission} 	${current_directory}/.*rc			~/
+	ln -s${ask_permission} 	${current_directory}/.*profile		~/
+	ln -s${ask_permission} 	${current_directory}/.gitconfig		~/
+	ln -s${ask_permission} 	${current_directory}/.shell 		~/
+	ln -s${ask_permission} 	${current_directory}/.ssh/config	~/.ssh/
 
-	ln -si 	${current_directory}/.*rc			~/
-	ln -si 	${current_directory}/.*profile		~/
-	ln -si 	${current_directory}/.gitconfig		~/
-	ln -si 	${current_directory}/.shell 		~/
-	ln -si 	${current_directory}/.ssh/config	~/.ssh/
-
-	ln -si 	${current_directory}/.local/share/backgrounds/custom/ghosts.jpg	~/.local/share/backgrounds/custom/
+	ln -s${ask_permission} 	${current_directory}/.local/share/backgrounds/custom/ghosts.jpg	~/.local/share/backgrounds/custom/
 
 	for config in .config/*
 	do
-		ln -si ${current_directory}/${config} ~/.config/
+		ln -s${ask_permission} ${current_directory}/${config} ~/.config/
 	done
 }
 
-main
+main $1
+
