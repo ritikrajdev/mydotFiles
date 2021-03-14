52066521106deb87fd01ebce07056d7cf37c9818
ritikrajdev <ritikrajdev761@gmail.com>
2021-03-14 07:28:39 +0530

    Automated script for link creation of files

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+main() {
+
+	local current_directory=$(dirname $(realpath $0))
+
+	echo $current_directory
+
+	ln -si 	${current_directory}/.*rc		~/
+	ln -si 	${current_directory}/.*profile		~/
+	ln -si 	${current_directory}/.shell 		~/
+	ln -si 	${current_directory}/.ssh/config	~/.ssh/
+
+	ln -si 	${current_directory}/.local/share/backgrounds/custom/ghosts.jpg	~/.local/share/backgrounds/custom/
+
+	for config in .config/*
+	do
+		ln -si ${current_directory}/${config} ~/.config/
+	done
+}
+
+main
