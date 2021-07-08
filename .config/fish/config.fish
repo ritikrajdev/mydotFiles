b883020bc5a53a7324da65ab78b863d5737a0c0f
ritikrajdev <ritikrajdev761@gmail.com>
2021-07-08 22:02:01 +0530

    Error Handling for pyenv and fzf key bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,7 @@ if test -e /usr/share/fish/vendor_functions.d/bass.fish
 	bass source /etc/profile
 end
 
-pyenv init - | source
+if test -e /bin/pyenv
+	/bin/pyenv init - | source
+end
 
--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,6 @@
 function fish_user_key_bindings
-	fzf_key_bindings
+	if test -e /usr/share/fish/vendor_functions.d/fzf_key_bindings.fish
+		fzf_key_bindings
+	end
 end
 
