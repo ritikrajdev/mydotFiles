cc033bb77d6385476bd0f87395b56a1355321bc9
ritikrajdev <ritikrajdev761@gmail.com>
2021-07-09 01:33:17 +0530

    Reduntant line removal

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,4 +1,3 @@
-# Defined in /tmp/fish.pmEfrS/fish_prompt.fish @ line 2
 function fish_prompt --description 'Write out the prompt'
 	set	-l	prompt (set_color green)"$USER@"(prompt_hostname)(set_color $fish_color_normal)
 	set	-la	prompt (set_color blue)(basename (prompt_pwd))(set_color $fish_color_normal)
